bcaf856dbe23c7547ec638f6db082b9225f4074b
Jordomav <jordomav@gmail.com>
2016-08-03 23:49:38 -0700

    fixed issue where red was misplaced in output pin number

--- sketch_aug02a/sketch_aug02a.ino
+++ sketch_aug02a/sketch_aug02a.ino
@@ -1,6 +1,6 @@
-#define RED 3
+#define BLUE 3
 #define GREEN 5
-#define BLUE 6
+#define RED 6
 
 int redNumber;
 int greenNumber;
@@ -16,17 +16,20 @@ void setup () {
   }
 
 void loop () {
-  redNumber = 0;
+  redNumber = 255;
   greenNumber = 0;
   blueNumber = 0;
+  analogWrite(RED, 255);
+  analogWrite(GREEN, 140);
+  analogWrite(BLUE, 0);
 
-  analogWrite(RED, 0);
-  delay(1000);
-  analogWrite(GREEN, 0);
-  delay(1000);
-  analogWrite(GREEN, 100);
-  analogWrite(RED, 59);
-  delay(1000);
-  analogWrite(BLUE, 100);
-  delay(1000);
+//  analogWrite(RED, 0);
+//  delay(1000);
+//  analogWrite(GREEN, 0);
+//  delay(1000);
+//  analogWrite(GREEN, 100);
+//  analogWrite(RED, 59);
+//  delay(1000);
+//  analogWrite(BLUE, 100);
+//  delay(1000);
   }
